d2e4cb4ded36c4b84b6b0409a08fcf516942c14a
Yabe12 <yeabsirabehailu92@gmail.com>
2025-02-14 21:45:15 +0300

    Update home page layout and new ui componenet for taskfy app

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -3,19 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wellcome to task app</title>
+    <title>Welcome to Task App</title>
     <script src="https://cdn.tailwindcss.com"></script> 
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
 
     <div class="bg-white p-8 rounded-lg shadow-md text-center w-96">
-        <!-- Profile Image -->
-        <img src="https://imgs.search.brave.com/h7dFgfTthSL5khfRBRczsbWLTr8e92gEzk8ONWMbUbs/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hbWVy/aWNhbmFydGRlY29y/LmNvbS9jZG4vc2hv/cC9maWxlcy81NTMw/NzFXRUJfNi5qcGc_/dj0xNzM0NTYzNzQ0/JndpZHRoPTUzMw" alt="Profile Image" class="mx-auto rounded-full shadow-lg">
+    
+        <img src="https://media.istockphoto.com/id/1589794103/photo/school-supplies-icon-school-analog-clock-3d-render.jpg?s=2048x2048&w=is&k=20&
+        <h1 class="text-4xl font-extrabold text-gray-800 mb-4">Welcome to Task App</h1>
 
-        <!-- Text -->
-        <h1 class="text-2xl font-bold mt-4">Smart task managment</h1>
+        <h2 class="text-2xl font-semibold text-gray-600 mb-6">Smart Task Management</h2>
 
-        <!-- Button -->
         <a href="#" class="mt-4 inline-flex items-center bg-blue-500 text-white px-5 py-2 rounded-lg hover:bg-blue-600 transition">
             Continue
             <svg class="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
