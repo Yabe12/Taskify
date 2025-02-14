b3e56783f2351bca5955a03325b8ec8b62ffc5ee
Yabe12 <yeabsirabehailu92@gmail.com>
2025-02-14 23:11:11 +0300

    feat: Add php artisan make:controller Auth/ForgotPasswordController for handling password rest

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,8 @@ Route::get('/', function () {
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 
+
+Route::get('forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail']);
+Route::get('reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('reset-password', [ResetPasswordController::class, 'reset']);
