f79ee851401535fcd524b5a987a994f62f188b7d
Yabe12 <yeabsirabehailu92@gmail.com>
2025-02-14 22:48:43 +0300

    refctor: move login.blade.php to resource/views/auth folder

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -10,12 +10,16 @@
 
     <div class="bg-white p-8 rounded-lg shadow-md text-center w-96">
     
-        <img src="https://media.istockphoto.com/id/1589794103/photo/school-supplies-icon-school-analog-clock-3d-render.jpg?s=2048x2048&w=is&k=20&
+        <img src="https://media.istockphoto.com/id/1589794103/photo/school-supplies-icon-school-analog-clock-3d-render.jpg?s=2048x2048&w=is&k=20&c=0WBUOC5TxF-Ncdk49FXQmBNAXhNAiQnpXuZGr8PrFZ4=" alt="Profile Image" class="mx-auto rounded-full shadow-lg mb-6">
+
+        <!-- Main Title: Large and Bold -->
         <h1 class="text-4xl font-extrabold text-gray-800 mb-4">Welcome to Task App</h1>
 
+        <!-- Subheading: Medium and Bold -->
         <h2 class="text-2xl font-semibold text-gray-600 mb-6">Smart Task Management</h2>
 
-        <a href="#" class="mt-4 inline-flex items-center bg-blue-500 text-white px-5 py-2 rounded-lg hover:bg-blue-600 transition">
+        <!-- Button -->
+        <a href="{{route('login')}}" class="mt-4 inline-flex items-center bg-blue-500 text-white px-5 py-2 rounded-lg hover:bg-blue-600 transition">
             Continue
             <svg class="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\LoginController;
 Route::get('/', function () {
     return view('Home');
 });
+
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
+
