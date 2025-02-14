8e84fbab042100247532551ad2ed8f32469c4b84
Yabe12 <yeabsirabehailu92@gmail.com>
2025-02-14 22:18:24 +0300

    feat: create logincontroller to handle login functinality

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Home');
 });
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
