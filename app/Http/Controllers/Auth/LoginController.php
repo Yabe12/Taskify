49c9abe4b822ef560e568ad728f30129aaaba5eb
Yabe12 <yeabsirabehailu92@gmail.com>
2025-02-14 22:20:28 +0300

    feat: Implement Logincontroller with login and authentification login in taskfy app

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,5 +7,24 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    // Handle login request
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return redirect()->intended('/dashboard'); // Redirect to dashboard or another page
+        }
+
+        return back()->withErrors(['email' => 'Invalid credentials']);
+    }
 }
